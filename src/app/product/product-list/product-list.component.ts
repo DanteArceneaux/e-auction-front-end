0f55e2822f22d24fcbee20479a5382b23c676aea
DanteArceneaux <90316637+DanteArceneaux@users.noreply.github.com>
2022-12-08 14:18:03 +0700

    created product model interface

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product',
@@ -8,7 +9,7 @@ import { Component } from '@angular/core';
 export class ProductListComponent {
 
 
-products: any[] = [];
+products: Product[] = [];
 
 
 
---
+++ src/app/product/product.model.ts
@@ -0,0 +1,10 @@
+
+
+export interface Product  {
+    productName: string;
+    shortDescription: string;
+    detailedDescription: string;
+    productCategory: string;
+    startingPrice: number;
+    bidEndDate: Date;
+}
